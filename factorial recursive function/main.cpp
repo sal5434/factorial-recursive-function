8c19a49a73407be68d620ab8158f2e5cad955f8d
Sarah Lakos <jaberjaw789@Sarahs-MacBook-Pro-2.local>
2012-12-01 16:21:15 -0500

    Initial Commit

---
+++ factorial recursive function/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  factorial recursive function
+//
+//  Created by Sarah Lakos on 12/1/12.
+//  Copyright (c) 2012 Sarah Lakos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
